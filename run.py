8eb3fc142c423563eb5f69cc2417551131e4418e
Sara Smith <saraa.smith@hotmail.com>
2023-04-30 13:18:29 +0000

    Center print statements in functions

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ def get_username():
     """
     Ask the player for a username
     """
-    username = input("Please enter a username: ")
+    username = input("                  Please enter a username: ")
     validate_username(username)
 
     
@@ -17,13 +17,16 @@ def validate_username(data):
                 "You need to enter a username")
         if len(data) < 2:
             raise ValueError(
-                "The username needs to be at least two characters long")
+                "The username needs to be at least 2 characters long.")
+        if len(data) > 10:
+            raise ValueError(
+                "The username cannot be longer than 10 characters.")
     except ValueError as e:
         print(f"\nInvalid data: {e}. Please try again.") 
         valid = False
     
     if valid:
-        print("Valid username")
+        print("\n                   Valid username. Lets play!\n\n")
     if valid is False:
         get_username()
 
@@ -32,20 +35,21 @@ def main():
     """
     Run all functions
     """
-    print("\nTTTTTTTTTT   RRRR    IIIIII  VV       VV  IIIIII       AAA")
+    print("\n\nTTTTTTTTTT   RRRR    IIIIII  VV       VV  IIIIII       AAA")
     print("    TT       R   R     II     VV     VV     II        AA AA")
     print("    TT       R  R      II      VV   VV      II       AA   AA")
     print("    TT       RRR       II       VV VV       II      AAAAAAAAA")
     print("    TT       R  R      II        VVV        II     AA       AA")
     print("    TT       R   R   IIIIII       V       IIIIII  AA         AA\n")
 
-    print("Did you know that ......\n")
-    print("                       Want to learn more?                      ")
-    print("          Test your trivia knowledge with a quiz game!          ")
+    print("----------------------------------------------------------------")
+    print("    Did you know that shrimps have their heart in the brain?    \n")
+    print("                       Want to learn more?                      \n")
+    print("            Test your trivia knowledge with a quiz!             ")
     print("----------------------------------------------------------------\n")
 
     get_username()
-    #game_options()
+    # game_options()
     
 
 main()
