406a530b1b2f14857ee6c1e3adcea8ad447e03cf
Sara Smith <saraa.smith@hotmail.com>
2023-04-30 09:49:19 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
 
-Welcome USER_NAME,
-
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **August 17, 2021**
 
 ## Reminders
 
 * Your code must be placed in the `run.py` file
 * Your dependencies must be placed in the `requirements.txt` file
 * Do not edit any of the other files or your code may not deploy properly
+* The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
 
 ## Creating the Heroku app
 
@@ -23,9 +20,3 @@ If you have credentials, such as in the Love Sandwiches project, you must create
 
 Connect your GitHub repository and deploy as normal.
 
-## Constraints
-
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
-
------
-Happy coding!
--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_user_name():
+    """
+    Introduce the game and ask for username
+    """
+    print("\nTTTTTTTTTT   RRRR    IIIIII  VV       VV  IIIIII       AAA")
+    print("    TT       R   R     II     VV     VV     II        AA AA")
+    print("    TT       R  R      II      VV   VV      II       AA   AA")
+    print("    TT       RRR       II       VV VV       II      AAAAAAAAA")
+    print("    TT       R  R      II        VVV        II     AA       AA")
+    print("    TT       R   R   IIIIII       V       IIIIII  AA         AA\n")
+
+    print("Did you know that ......\n")
+    print("                       Want to learn more?                      ")
+    print("          Test your trivia knowledge with a quiz game!          ")
+    print("----------------------------------------------------------------\n")
+    username = input("Please enter your username: ")
+
+
+def game_options():
+
+
+def main():
+    """
+    Run all functions
+    """
+    get_user_name()
+    game_options()
+
+
+main()
