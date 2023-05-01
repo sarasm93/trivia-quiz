accf89319f8c6c86461e6d3546289e520aeea6e1
Sara Smith <saraa.smith@hotmail.com>
2023-05-01 15:43:50 +0000

    Add first parts of functions to display and validate questions and answers

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ def get_username():
     validate_username(username)
 
 
-def validate_username(data):
+def validate_username(input_name):
     """
     Validate username by checking that minimum two and maximum 10
     characteres were entered.
@@ -14,17 +14,17 @@ def validate_username(data):
     valid = True
 
     try:
-        for blank in data:
+        for blank in input_name:
             if (blank.isspace()) == True:
                 raise ValueError(
                     " It cannot contain blank spaces")
-        if len(data) == 0:
+        if len(input_name) == 0:
             raise ValueError(
                 " You didn't enter a username")
-        if len(data) < 2:
+        if len(input_name) < 2:
             raise ValueError(
                 " It needs to be at least 2 characters")
-        if len(data) > 10:
+        if len(input_name) > 10:
             raise ValueError(
                 " It cannot be longer than 10 characters")
     except ValueError as e:
@@ -40,7 +40,7 @@ def validate_username(data):
 # WANT TO LET THE GET_USERNAME FUNCTION LINGER A FEW SECONDS
 
 
-def choose_game():
+def choose_option():
     # WANT TO START A NEW PAGE (REMOVE INTRO)
     """
     Display options menu, to choose game or show score board
@@ -52,13 +52,13 @@ def choose_game():
     print("[g] Trivia about Geography")
     print("[v] View score board\n\n")
     player_choice = input("Enter your choice: ")
-    validate_game(player_choice)
+    validate_choice(player_choice)
 
 
-def validate_game(data):
+def validate_choice(input_choice):
     """
     Function to run a game or show the score board, based on 
-    the choice of the user.
+    the choice of the player.
     """
     option_one = "s"
     option_two = "m"
@@ -66,18 +66,62 @@ def validate_game(data):
     option_four = "v"
     
     try:
-        if data in (option_one, option_two, option_three, option_four): 
-            games()
+        if input_choice in (option_one, option_two, option_three, option_four):  
+            options(input_choice)
         else:
             raise ValueError()
     except ValueError:
         print("\nInvalid choice. Enter s, m, g or v to make a choice.")
         input("Press enter to choose again.")
-        choose_game()
+        choose_option()
 
     
+def options(input_choice):
+    """
+    Displaying questions in chosen quiz or score board, based on valid choice by player. 
+    """
+    # WANT TO START A NEW PAGE (REMOVE OPTIONS)
 
+    if input_choice == "s":
+        run_science()
+    if input_choice == "m":
+        run_movies()
+    if input_choice == "g":
+        run_geography()
+    if input_choice == "v":
+        run_score_board()
 
+def run_science():
+    """
+    """
+    valid = True
+
+    try:
+        for question in question_list:
+            correct_answer = question.pop()
+            for item in question:
+                print(item)
+                player_answer = input("Enter your answer: ")
+                validate_answer(player_answer)
+
+def validate_answer(input_answer):
+    """
+    Validating player answer to question
+    """
+    answer_one = "1"
+    answer_two = "2"
+    answer_three = "3"
+    
+    try:
+        if input_anwser in (answer_one, answer_two, answer_three):  
+            options(input_choice)
+        else:
+            raise ValueError()
+    except ValueError:
+        print("\nInvalid answer. Enter 1, 2 or 3 to answer.")
+        input("Press enter to try again.")
+        options(input_choice)
+        # Another function is supposed to be called (when it is built)
 
 def main():
     """
@@ -97,7 +141,7 @@ def main():
     print("----------------------------------------------------------------\n")
 
     get_username()
-    choose_game()
+    choose_option()
     
 
 main()
