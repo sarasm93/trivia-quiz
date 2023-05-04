fa0448d1f3f8975be6ad27cb58c1e05fccd6db04
Sara Smith <saraa.smith@hotmail.com>
2023-05-04 11:26:25 +0000

    Change score board dictionarys to lists and add headings for table as list

--- run.py
+++ run.py
@@ -1,64 +1,35 @@
 import os
 import time
+from tabulate import tabulate as tb
+
 username = 0
 
 score_boards = [
-    {"Jenny": 10, "GameGuru": 9, "lulu": 9, "Carl": 8}, 
-    {"lulu": 10, "GameGuru": 8, "Jenny": 7, "Carl": 7}, 
-    {"Jenny": 9, "Carl": 8, "lulu": 6, "GameGuru": 5}
+    [["SCIENCE TRIVIA", "Player", "Score"], {"Jenny": 10, "GameGuru": 7, "lulu": 7, "Carl": 6}],
+    [["MOVIE TRIVIA", "Player", "Score"], {"lulu": 8, "GameGuru": 8, "Jenny": 7, "Carl": 4}],
+    [["GEOGRAPHY TRIVIA", "Player", "Score"], {"Jenny": 9, "Carl": 5, "lulu": 4, "GameGuru": 4}]
     ]
 
 
 def delay(input_time):
     """
-    When this function is called, the next code to be displayed is 
-    delayed. This makes user experiance better as the player have more 
+    When this function is called, the next code to be displayed is
+    delayed. This makes user experiance better as the player have more
     time to see what happens.
     """
     time.sleep(input_time)
 
 
-# The code on line XXXXXXXXXXX(os.system(clear)) is taken from this Stackoverflow page. 
-# https://stackoverflow.com/questions/4810537/how-to-clear-the-screen-in-python
 def clear_screen():
     """
     When this function is called, the screen is cleared. This makes user 
     experiance better as the the terminal is not so cluttered with text.
     """
+    # The code on line XXXXXXXXXXXX is taken from this Stackoverflow page. 
+    # https://stackoverflow.com/questions/4810537/how-to-clear-the-screen-in-python
     os.system('clear')
 
 
-def sort_score_board(input_board, input_index):
-    """
-    Function sorting player scores before showed to player. Only the dictionary
-    for the quiz played is updated.
-    """
-    dictionary = input_board[input_index]
-    sorted_board = {}
-    sorted_scores = sorted(dictionary.values(), reverse=True)
-    for score in sorted_scores:
-        for key in dictionary.keys():
-            if dictionary[key] == score:
-                sorted_board[key] = dictionary[key]
-
-    dictionary.clear()
-    dictionary.update(sorted_board)
- 
-
-def run_score_boards():
-    """
-    Function called when player has choosen to view score board in 
-    the options function. Scores are sorted by sort_score_board()-function
-    before showed. 
-    """
-    for board in score_boards:
-        for score in board.items():
-            #list(score) FORTSÄTT HÄÄÄR
-            print(score)
-    input("\n\nPress enter to get back to menu")
-    display_options()
-
-    
 def get_username():
     """
     Ask the player for a username.
@@ -77,11 +48,11 @@ def validate_username(input_name):
 
     try:
         for blank in input_name:
-            if (blank.isspace()) == True:
+            if (blank.isspace()) is True:
                 raise ValueError(
                     " It cannot contain blank spaces")
         for board in score_boards:
-            taken_usernames = board.keys()
+            taken_usernames = board[1].keys()
             for name in taken_usernames:
                 if input_name == name:
                     raise ValueError(
@@ -103,7 +74,7 @@ def validate_username(input_name):
     if valid:
         print("\n                           Lets play!\n\n")
         delay(1.2)
-    if valid == False:
+    if valid is False:
         get_username()
 
 
@@ -137,17 +108,17 @@ def validate_choice(input_choice, valid_options):
     """
     Function to run a game or show the score board, based on
     the choice of the player. To make the valid options print nicely
-    to the player, the valid options list re-designed by (1) splitting it,
+    to the player, the valid options list is re-designed by (1) splitting it,
     (2) adding "or" to the second to last position, (3) putting it together
     again and (4) removing all '[]' and ','.
     """
-    input_choice = input_choice.lower()
-    valid_options_string = str(valid_options)
-
     # Code on line XXXXXXXXXXX is taken from the below Geeksforgeeks.org page.
     # The code has been adjusted to fit to this program by changing variable
     # names and the position to insert new word "or".
     # https://www.geeksforgeeks.org/python-add-phrase-in-middle-of-string/
+    input_choice = input_choice.lower()
+    valid_options_string = str(valid_options)
+
     add_word = "or"
     words = valid_options_string.split()
     position = len(words) - 1
@@ -164,18 +135,54 @@ def validate_choice(input_choice, valid_options):
         return False
 
 
+def sort_score_board(input_board, list_index, dict_index):
+    """
+    Function sorting dictionary of scores in score board, each time a player
+    finishes a quiz. Only the dictionary for the quiz that the player just
+    played is sorted.
+    """
+    # Code on line XXXXXXXXXXX is taken from the below page to loop through
+    # a dictionary of scores. The code has been adjusted to fit to this
+    # program by changing variable names.
+    # https://stackabuse.com/how-to-sort-dictionary-by-value-in-python/
+    dictionary = input_board[list_index][dict_index]
+    sorted_board = {}
+    sorted_scores = sorted(dictionary.values(), reverse=True)
+    for score in sorted_scores:
+        for key in dictionary.keys():
+            if dictionary[key] == score:
+                sorted_board[key] = dictionary[key]
+
+    dictionary.clear()
+    dictionary.update(sorted_board)
+
+
+def run_score_boards():
+    """
+    Function called when player has choosen to view score board in
+    the options function. Scores are sorted by sort_score_board()-function
+    before showed.
+    """
+    for board in score_boards:
+        for score in board.items():
+            player_score_list = list(score) 
+            print(player_score_list)
+    
+    input("\n\nPress enter to get back to menu")
+    display_options()
+
+
 def run_option(input_choice):
     """
     Initiate the right quiz, show score board or quit game, based
-    on valid choice by player. 
+    on valid choice by player.
     """
     input_choice = input_choice.lower()
 
     if input_choice == "s":
         clear_screen()
-        quiz_name = "SCIENCE TRIVIA"
-        print(f"\n\n{quiz_name}\n\n")
-        # Last item in each nested list is the correct answer for that question.
+        print(f"\n\n{score_boards[0][0][0]}\n\n")
+        # The last item in each nested list of questions is the correct answer for that question.
         science_questions = [
             ["Question 1: What....", "[1] Option 1", "[2] Option 2", "[3] Option 3\n", "1"],
             ["Question 2: What....", "[1] Option 1", "[2] Option 2", "[3] Option 3\n", "3"],
@@ -185,16 +192,15 @@ def run_option(input_choice):
             ["Question 6: What....", "[1] Option 1", "[2] Option 2", "[3] Option 3\n", "3"]
         ]
         score = run_quiz(science_questions)
-        score_boards[0].update([(username, score)])
-        sort_score_board(score_boards, 0)
-        print(f"\nYou finished the {quiz_name} quiz! Well done! Your score: {score}\n")
+        score_boards[0][1].update([(username, score)])
+        sort_score_board(score_boards, 0, 1)
+        print(f"\nYou finished the {score_boards[0][0][0]} quiz! Well done! Your score: {score}\n")
         input("Press enter to get back to menu")
         display_options()
 
     if input_choice == "m":
         clear_screen()
-        quiz_name = "MOVIE TRIVIA"
-        print(f"\n\n{quiz_name}\n\n")
+        print(f"\n\n{score_boards[1][0][0]}\n\n")
         movie_questions = [
             ["Question 1: What....", "[1] Option 1", "[2] Option 2", "[3] Option 3\n", "1"],
             ["Question 2: What....", "[1] Option 1", "[2] Option 2", "[3] Option 3\n", "3"],
@@ -204,16 +210,15 @@ def run_option(input_choice):
             ["Question 6: What....", "[1] Option 1", "[2] Option 2", "[3] Option 3\n", "3"]
         ]
         score = run_quiz(movie_questions)
-        score_boards[1].update([(username, score)])
-        sort_score_board(score_boards, 1)
-        print(f"\nYou finished the {quiz_name} quiz! Well done! Your score: {score}\n")
+        score_boards[1][1].update([(username, score)])
+        sort_score_board(score_boards, 1, 1)
+        print(f"\nYou finished the {score_boards[1][0][0]} quiz! Well done! Your score: {score}\n")
         input("Press enter to get back to menu")
         display_options()
 
     if input_choice == "g":
         clear_screen()
-        quiz_name = "GEOGRAPHY TRIVIA"
-        print(f"\n\n{quiz_name}\n\n")
+        print(f"\n\n{score_boards[2][0][0]}\n\n")
         geography_questions = [
             ["Question 1: What....", "[1] Option 1", "[2] Option 2", "[3] Option 3\n", "1"],
             ["Question 2: What....", "[1] Option 1", "[2] Option 2", "[3] Option 3\n", "3"],
@@ -223,9 +228,9 @@ def run_option(input_choice):
             ["Question 6: What....", "[1] Option 1", "[2] Option 2", "[3] Option 3\n", "3"]
         ]
         score = run_quiz(geography_questions)
-        score_boards[2].update([(username, score)])
-        sort_score_board(score_boards, 2)
-        print(f"\nYou finished the {quiz_name} quiz! Well done! Your score: {score}\n")
+        score_boards[2][1].update([(username, score)])
+        sort_score_board(score_boards, 2, 1)
+        print(f"\nYou finished the {score_boards[2][0][0]} quiz! Well done! Your score: {score}\n")
         input("Press enter to get back to menu")
         display_options()
 
@@ -290,5 +295,6 @@ def main():
 
     get_username()
     display_options()
+
 
 main()
